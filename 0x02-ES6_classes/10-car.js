d454be89531beabeeb1a5d162b99c0ddffdad7e9
Nazzcodek <bellnas09@gmail.com>
2024-01-14 11:08:37 +0000

    task 9and10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+export default class Car {
+  /* eslint-disable no-underscore-dangle */
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
