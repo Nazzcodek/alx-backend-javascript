57eb9e765eb141da9fdcbc7ca9d67243c7b38bac
Nazzcodek <65385684+Nazzcodek@users.noreply.github.com>
2024-04-29 13:08:23 +0100

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/0-calcul.js
+++
@@ -1,8 +0,0 @@
-// the calculation function
-
-
-module.exports = function calculateNumber(a, b) {
-  aRound = Math.round(a);
-  bRound = Math.round(b);
-  return aRound + bRound;
-}
--- 0x06-unittests_in_js/4-payment.js
+++
@@ -1,6 +0,0 @@
-Utils = require('./utils');
-
-module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const output = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${output}`);
-}
