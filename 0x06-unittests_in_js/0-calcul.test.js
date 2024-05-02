4056712d886cf9e7985160211aeb47d6811b43f9
Nazzcodek <bellnas09@gmail.com>
2024-05-02 10:11:36 +0000

    testing for more cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,9 +4,15 @@ const calculateNumber = require("./0-calcul.js");
 
 describe( 'calculateNumber', function (){
   it('Should add rounded number', function () {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('Should round and add float and int', function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should round two float', function () {
     assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should round up two float', function () {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
